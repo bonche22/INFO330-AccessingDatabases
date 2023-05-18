e9271ca409ec99ceef3879001b38c8b1866e6973
bonche22 <bonche22@uw.edu>
2023-05-17 22:15:31 -0700

    debugging

--- Bonnie_TeamAnalyzer.py
+++ Bonnie_TeamAnalyzer.py
@@ -1,27 +1,27 @@
-import sqlite3  # This is the package for all sqlite3 access in Python
-import sys      # This helps with command-line parameters
+import sqlite3
+import sys
 
 # All the "against" column suffixes:
-types = ["bug","dark","dragon","electric","fairy","fight",
-    "fire","flying","ghost","grass","ground","ice","normal",
-    "poison","psychic","rock","steel","water"]
+types = ["bug", "dark", "dragon", "electric", "fairy", "fight",
+         "fire", "flying", "ghost", "grass", "ground", "ice", "normal",
+         "poison", "psychic", "rock", "steel", "water"]
 
 # Connect to the pokemon.sqlite database
 conn = sqlite3.connect('pokemon.sqlite')
 c = conn.cursor()
 
-# Take six parameters on the command-line
-if len(sys.argv) < 6:
+# Take six parameters from the command-line
+if len(sys.argv) < 7:
     print("You must give me six Pokemon to analyze!")
     sys.exit()
 
 team = []
-for i, arg in enumerate(sys.argv):
-    if i == 0:
-        continue
+for i in range(1, 7):
+    arg = sys.argv[i]
 
     # Analyze the pokemon whose pokedex_number is in "arg"
-    c.execute("SELECT against_bug, against_dark, against_dragon, against_electric, against_fairy, against_fight, against_fire, against_flying, against_ghost, against_grass, against_ground, against_ice, against_normal, against_poison, against_psychic, against_rock, against_steel, against_water FROM imported_pokemon_data WHERE pokedex_number = ?", (arg,))
+    query = "SELECT name, type1, type2, " + ", ".join(["against_" + t for t in types]) + " FROM imported_pokemon_data WHERE pokedex_number = ?"
+    c.execute(query, (arg,))
     pokemon = c.fetchone()
 
     if not pokemon:
@@ -29,30 +29,18 @@ for i, arg in enumerate(sys.argv):
         continue
 
     # Extract the values for the types of the current Pokemon
-    type1 = pokemon[0] if pokemon[0] is not None else 1
-    type2 = pokemon[1] if pokemon[1] is not None else 1
-    types = [type1, type2]
+    name = pokemon[0]
+    type1 = pokemon[1] if pokemon[1] is not None else ""
+    type2 = pokemon[2] if pokemon[2] is not None else ""
+    against_types = pokemon[3:]
 
     # Determine the types that this Pokemon is strong and weak against
-    strong_against = []
-    weak_against = []
-    for j in range(len(types)):
-        if types[j] > 1:
-            strong_against.append(types[j])
-        elif types[j] < 1:
-            weak_against.append(types[j])
-
-
-    for j in range(len(types)):
-    if types_val[j] > 1:
-        strong_against.append(types[j])
-    elif types_val[j] < 1:
-        weak_against.append(types[j])
-
+    strong_against = [types[j] for j in range(len(against_types)) if float(against_types[j]) > 1]
+    weak_against = [types[j] for j in range(len(against_types)) if float(against_types[j]) < 1]
 
     # Print out the results
     print("Analyzing " + arg)
-    print(pokemon[2] + " is strong against " + str(strong_against) + " but weak against " + str(weak_against))
+    print(name + " (" + type1 + " " + type2 + ") is strong against " + str(strong_against) + " but weak against " + str(weak_against))
 
     team.append(int(arg))
 
